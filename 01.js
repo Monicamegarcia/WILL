839ed54e74390d5466e82d84eb87420ee23bce89
Monicamegarcia <monicamegarcia@gmail.com>
2023-01-18 21:08:52 -0300

    first

--- 01.js
+++ 01.js
@@ -11,7 +11,10 @@ function soloNumeros(array) {
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
-aa
+  
+let newArray = array.filter(numero => Number.isInteger(numero)
+  );
+return newArray;
 }
 
 // No modifiques nada debajo de esta linea //
